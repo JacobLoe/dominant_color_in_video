6af677cf8784664f81d4da5a8371bdda87d3cd85
JacobLoe <Jcbl423@gmail.com>
2018-08-22 23:37:05 +0200

    started unit tests

--- unit_tests.py
+++ unit_tests.py
@@ -1,15 +1,23 @@
 #####################################################
 ## libraries
 #####################################################
-import cv2
+import extract_dominant_color
 import numpy as np
-import pandas as pd
-from scipy.spatial.distance import euclidean
-from sklearn.neighbors import KDTree
-import time
-import argparse
-import zipfile
-import xml.etree.ElementTree as ET
 ######################################
+#def test_bins_to_df():
+#    
+#    pd.DataFrame()
+#    assert bins_to_df()==
+def test_frames():
+    black=np.array([[[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]],
+                [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]],
+                [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]])
+    red=np.array([[[255,0,0],[255,0,0],[255,0,0],[255,0,0],[255,0,0]],
+             [[255,0,0],[255,0,0],[255,0,0],[255,0,0],[255,0,0]],
+              [[255,0,0],[255,0,0],[255,0,0],[255,0,0],[255,0,0]]])
+    frames_black=[black,black,black]
+    frames_red=[black,red,black]
+#    assert frames_black[0].all()==frames_red[0].all()
+    assert frames_black[1].all()==frames_red[1].all()
 if __name__ == "__main__":
-   print(5)
+   print('unit_tests')
